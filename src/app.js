996762036e3617775843815faec252af52aa5804
Joshua <jdj369@gmail.com>
2023-07-10 15:03:53 -0400

    404,405 and 500 error handlers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
-const movieRouter = require("./movies/movies.router")
+const movieRouter = require("./movies/movies.router");
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
 const app = express();
 
 app.use("/movies", movieRouter);
+
+app.use(notFound)
+app.use(errorHandler);
+
 module.exports = app;
--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+const methodNotAllowed = require("../errors/methodNotAllowed");
 const {list} = require("./movies.controller")
 
-router.route("/").get(list)
+router.route("/").get(list).all(methodNotAllowed)
 
 module.exports = router
