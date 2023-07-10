80d745bcca9a56e29e3cd2b56a97a8a156a74fb9
Joshua <jdj369@gmail.com>
2023-07-10 16:53:24 -0400

    removed a console.log

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -38,7 +38,6 @@ async function list(req, res, _next) {
   const { is_showing } = req.query;
   if (is_showing === "true") {
     const data = await listShowingMovies();
-    console.log(data.length);
     res.send({ data });
   } else {
     const data = await listMovies();
