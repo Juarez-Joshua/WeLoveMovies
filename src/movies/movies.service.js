eb3eca6c3355744b95a1222204312864ed03b9da
Joshua <jdj369@gmail.com>
2023-07-11 15:16:37 -0400

    fixed get /movies with query

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,22 +1,21 @@
 const knex = require("../db/connection");
 const mapProperties = require("../utils/map-properties");
 
-const addCritic = mapProperties(
-  {
-    critic_id: "critic.critic_id",
-    preferred_name: "critic.preferred_name",
-    surname: "critic.surname",
-    organization_name: "critic.organization_name",
-    created_at: "critic.created_at",
-    updated_at: "critic.updated_at"
-})
+const addCritic = mapProperties({
+  critic_id: "critic.critic_id",
+  preferred_name: "critic.preferred_name",
+  surname: "critic.surname",
+  organization_name: "critic.organization_name",
+  created_at: "critic.created_at",
+  updated_at: "critic.updated_at",
+});
 
 function movieReviews(movieId) {
   return knex("reviews as r")
     .join("critics as c", "c.critic_id", "r.critic_id")
     .select("*")
-    .where({ "r.movie_id" : movieId })
-    .then(data => data.map(addCritic));
+    .where({ "r.movie_id": movieId })
+    .then((data) => data.map(addCritic));
 }
 
 function findTheatersForMovie(movieId) {
@@ -37,8 +36,15 @@ function listMovies() {
 function listShowingMovies() {
   return knex("movies as m")
     .join("movies_theaters as t", "m.movie_id", "t.movie_id")
-    .select("*")
-    .where({ is_showing: true })
+    .select(
+      "m.movie_id",
+      "m.title",
+      "m.runtime_in_minutes",
+      "m.rating",
+      "m.description",
+      "m.image_url"
+    )
+    .where({ "t.is_showing": true })
     .groupBy("m.movie_id");
 }
 module.exports = {
